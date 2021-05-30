cb9f84d4e7d1705436b55c170b0ed377bee91df0
Santiago Triana <a.trianaa@uniandes.edu.co>
2021-05-29 23:46:28 -0500

    prueba

--- src/main/main.java
+++ src/main/main.java
@@ -1,5 +1,7 @@
 package main;
 
+import javax.swing.JOptionPane;
+
 import problema_A.Problema_a;
 import problema_A.interfaz_problema_a;
 
@@ -9,8 +11,11 @@ public class main {
 	private static interfaz_problema_a interfaz_problema_a;
 
 	public static void main(String[] args) {
-		interfaz_problema_a = new interfaz_problema_a();
-		//problema_a = new Problema_a();
+		String[] botones = { "Problema A", "Problema B"};
+	    int rta = JOptionPane.showOptionDialog(null, "¿Que solución desea correr?", "Solucion?",
+	        JOptionPane.INFORMATION_MESSAGE, 0, null, botones, botones[0]);
+	    if(rta==0) interfaz_problema_a = new interfaz_problema_a();
+	    else if(rta==1);
 	}
 	
 
